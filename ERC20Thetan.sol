9cff0691125c9f791827e9208fa9bfaa7b69e18b
Hieu Hiep Nguyen <peihsen.doyle@gmail.com>
2021-09-02 22:52:26 +0700

    - add BEP20 and ERC20 token smart contracts.

---
+++ ERC20Thetan.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract THGToken is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable {
+    
+    using SafeMath for uint;
+
+    uint256 private totalTokens;
+
+    constructor() ERC20("Thetan Gem", "THG") {
+        totalTokens = 420 * 10 ** 6 * 10 ** uint256(decimals()); // 420M
+        _mint(owner(), totalTokens);  
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function getBurnedAmountTotal() external view returns (uint256 _amount) {
+        return totalTokens.sub(totalSupply());
+    }
+}
